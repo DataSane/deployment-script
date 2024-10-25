0852b54ff554a0c1de34a54bcc39e0b6527108f2
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-10-25 15:21:56 -0300

    [FIX] :: altering the usermod again

--- deploy_datasane-app.sh
+++ deploy_datasane-app.sh
@@ -11,11 +11,17 @@ sudo apt install -y docker.io docker-compose
 docker --version
 docker-compose --version
 
+sudo usermod -aG docker $USER
+
 sudo systemctl start docker
 sudo systemctl enable docker
 
 echo "Cloning the project..."
-git clone https://www.github.com/datasane/datasane-app.git
+if [ -d "datasane-app" ]; then
+  echo "Directory datasane-app already exists. Removing it..."
+  rm -rf datasane-app
+fi
+git clone https://github.com/datasane/datasane-app.git
 cd datasane-app
 
 read -p "Enter AWS Access Key ID: " AWS_ACCESS_KEY_ID
@@ -23,4 +29,4 @@ read -p "Enter AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
 read -p "Enter AWS Region: " AWS_REGION
 
 echo "Starting Docker Compose..."
-sudo -E env "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" "AWS_REGION=$AWS_REGION" docker-compose up --build
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_REGION=$AWS_REGION docker-compose up --build

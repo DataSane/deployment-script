d07fbdb4a7064ed8a92a64897bb5fcaa123a34c6
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-10-25 15:48:11 -0300

    [CHORE]:: trying to use export

--- deploy_datasane-app.sh
+++ deploy_datasane-app.sh
@@ -22,6 +22,9 @@ read -p "Enter AWS Access Key ID: " AWS_ACCESS_KEY_ID
 read -p "Enter AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
 read -p "Enter AWS Session Token: " AWS_SESSION_TOKEN
 
-
 echo "Starting Docker Compose..."
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN sudo docker-compose up --build
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
+
+docker-compose up --build

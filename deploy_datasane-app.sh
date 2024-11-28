8dfb8c6cd20543983c588714827b35b16ce79de8
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-11-28 17:41:18 -0300

    [FIX] :: using specific Docker compose, by Docker.io

--- deploy_datasane-app.sh
+++ deploy_datasane-app.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo "[Starting deploy setup]"
 
 # Atualiza e faz upgrade do sistema
@@ -8,21 +6,21 @@ sudo apt update && sudo apt upgrade -y
 
 # Instala o Docker e o plugin do Docker Compose V2
 echo "Installing Docker and Docker Compose V2..."
-sudo apt install -y docker.io
-sudo apt install -y docker-compose
-sudo apt install -y docker-compose-plugin
+sudo apt install -y docker.io docker-compose-plugin
 
 # Verifica as versões instaladas
 docker --version
+
+# Inicia e habilita o serviço Docker
+# Verifica as versões instaladas
+docker --version
+docker-compose --version
 docker compose version
 
 # Inicia e habilita o serviço Docker
 echo "Starting Docker service..."
 sudo systemctl start docker
 sudo systemctl enable docker
-
-# Colocando Docker como root
-sudo usermod -aG docker $USER
 
 # Clona o projeto
 echo "Cloning the project..."
@@ -34,6 +32,7 @@ read -p "Enter AWS Access Key ID: " AWS_ACCESS_KEY_ID
 read -p "Enter AWS Secret Access Key: " AWS_SECRET_ACCESS_KEY
 read -p "Enter AWS Session Token: " AWS_SESSION_TOKEN
 
+echo "Starting Docker Compose..."
 # Define as variáveis de ambiente para as credenciais da AWS
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
@@ -41,4 +40,4 @@ export AWS_SESSION_TOKEN=$AWS_SESSION_TOKEN
 
 # Inicia o Docker Compose
 echo "Starting Docker Compose..."
-docker-compose up --build
+docker compose up --build
